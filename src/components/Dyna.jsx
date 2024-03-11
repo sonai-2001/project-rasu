f798f82672f9705ab4cf0b03e250a8c3ec55e78d
sonai-2001 <sonaibhadra11174@gmail.com>
2024-03-11 14:59:48 +0530

    navigate

--- src/components/Dyna.jsx
+++ src/components/Dyna.jsx
@@ -1,12 +1,20 @@
 import React from "react"
-import {useParams} from "react-router-dom"
+import {useParams,useVavigate} from "react-router-dom"
 function Dyna (){
     
  const x= useParams()
  const {name}=x
+ const  Navigate= useNavigate()
+ const handleClick=()=>{
+    Navigate(-1)
+ }
+       
     return (
        
-       <div className="mt-10">hi {name}</div>
+       <div className="mt-10 ">
+        <h1>hi {name}</h1>
+        <button onClick={handleClick} className="p-2 bg-red-200 rounded-md">go back</button>
+       </div>
         )
 }
 export default Dyna
